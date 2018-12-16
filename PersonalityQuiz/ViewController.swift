fe62ddff47397252faeb4917aeaf134ded21c91a
Rich Castagna <idev.tru@gmail.com>
2018-12-16 18:05:01 -0500

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Richard Castagna on 12/16/18.
+//  Copyright © 2018 Richard Castagna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
